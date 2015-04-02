1de12a7092dc6f0330bbfc5832f974831bffb339
OAlpha-HP000 <lnugentgibson@gmail.com>
2015-04-02 03:43:15 -0400

    working on browser

--- src/main/java/edu/fgcu/stesting/uiesg/browser/Browser.java
+++ src/main/java/edu/fgcu/stesting/uiesg/browser/Browser.java
@@ -1,7 +1,40 @@
 package edu.fgcu.stesting.uiesg.browser;
 
-public class Browser {
-	
-	// testing commit from user Brent Oliver
-    
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class Browser extends Application {
+
+	@Override
+	public void start( Stage primaryStage ) throws Exception {
+		WebView browser = new WebView();
+		WebEngine webEngine = browser.getEngine();
+		webEngine.load("http://10.0.0.1:80");
+		Button btn = new Button();
+		btn.setText("Say 'Hello World'");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle( ActionEvent event ) {
+				System.out.println("Hello World!");
+			}
+		});
+		StackPane root = new StackPane();
+		root.getChildren().add(browser);
+		Scene scene = new Scene(root, 300, 250);
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main( String[] args ) {
+		launch();
+	}
+
 }
