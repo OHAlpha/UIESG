e3536c597f5f468d4070d21c0f6275281e116554
brentoliver <oliver.brent@gmail.com>
2015-04-16 11:22:21 -0400

    added test method to BrowserTest

--- src/test/java/edu/fgcu/stesting/uiesg/browser/BrowserTest.java
+++ src/test/java/edu/fgcu/stesting/uiesg/browser/BrowserTest.java
@@ -1,4 +1,10 @@
 package edu.fgcu.stesting.uiesg.browser;
+
+import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.*;
 
 /***
@@ -21,12 +27,62 @@ public class BrowserTest {
 	public static void setup() {
 
 	}
-	
-	
-	
-	
-	
-	
-	
+
+	@Before
+	public void before() {
+		browser = new Browser();
+
+	}
+
+	@After
+	public void after() {
+
+	}
+
+	@AfterClass
+	public static void teardown() {
+
+	}
+
+	// unit tests for the browser
+
+	@Test
+	public void testStart() {
+		// code to test browser start function
+		throw new RuntimeException("test not implemented");
+	}
+
+	@Test
+	public void testUpdatePage() {
+		// code to test updatePage returns boolean btw
+		
+		browser.sed = null;
+		boolean tst = true;
+		URL url;
+		try {
+			url = new URL("https://www.google.com");
+			boolean bool = browser.updatePage(url);
+			assertEquals(tst, bool);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// test if (sed == null || !sed.getDomain().equalsIgnoreCase(domain)) 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
 
 } // end BrowserTest class
