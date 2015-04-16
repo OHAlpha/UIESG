f494a72eb4bfb26ddfb44280be8dae996b575059
brentoliver <oliver.brent@gmail.com>
2015-04-16 09:13:11 -0400

    added BrowserTest class and appropriate package

---
+++ src/test/java/edu/fgcu/stesting/uiesg/browser/BrowserTest.java
@@ -0,0 +1,32 @@
+package edu.fgcu.stesting.uiesg.browser;
+import org.junit.*;
+
+/***
+ * 
+ * Performs unit tests for the browser class
+ * 
+ * 
+ * @author beoliver
+ *
+ */
+public class BrowserTest {
+
+	Browser browser;
+
+	public BrowserTest() {
+
+	}
+
+	@BeforeClass
+	public static void setup() {
+
+	}
+	
+	
+	
+	
+	
+	
+	
+
+} // end BrowserTest class
