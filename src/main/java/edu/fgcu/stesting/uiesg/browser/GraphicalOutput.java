a5c005687a30e4678857721edc96843ea6489f01
brentoliver <oliver.brent@gmail.com>
2015-04-21 16:25:29 -0400

    updated GO

--- src/main/java/edu/fgcu/stesting/uiesg/browser/GraphicalOutput.java
+++ src/main/java/edu/fgcu/stesting/uiesg/browser/GraphicalOutput.java
@@ -5,11 +5,9 @@ import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 // class to output to the user the graphical information 
 // gathered from the web browser
@@ -22,12 +20,14 @@ public class GraphicalOutput extends Application {
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void start(Stage stage) throws Exception {
+		
 		// TODO Auto-generated method stub
-		final LineChart<Number, Number> sc = new LineChart<>(new NumberAxis(),
+		final LineChart<Number, Number> lc = new LineChart<>(new NumberAxis(),
 				new NumberAxis());
-
+		lc.setTitle("UIESG Data");
 		// series1 will consist of the points that
 		XYChart.Series series1 = new XYChart.Series();
 		series1.setName("Mouse events");
@@ -42,22 +42,24 @@ public class GraphicalOutput extends Application {
 		// test points for mouse movements
 		series2.getData().add(new XYChart.Data(5.2, 229.2));
 		series2.getData().add(new XYChart.Data(2.4, 37.6));
-		series2.getData().add(new XYChart.Data(6.4, 15.6));
+		series2.getData().add(new XYChart.Data(50, 15.6));
 
-		sc.setAnimated(false);
-		sc.setCreateSymbols(true);
+		lc.setAnimated(false);
+		lc.setCreateSymbols(true);
 
-		sc.getData().addAll(series1, series2);
+		lc.getData().addAll(series1, series2);
 
-		Scene scene = new Scene(sc, 500, 400);
+		Scene scene = new Scene(lc, 500, 400);
 		scene.getStylesheets().add(
 				getClass().getResource("chart.css").toExternalForm());
 		stage.setScene(scene);
 		stage.show();
+		
 	}
 
 	public static void main(String[] args) {
 		// run linechart
 		launch(args);
+		
 	}
 }
